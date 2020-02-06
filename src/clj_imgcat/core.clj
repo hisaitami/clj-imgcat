90fb3ddd32609b69c2d49505e0b6df018fc488e4
hisaitami <hisaitami@gmail.com>
2020-02-07 04:16:15 +0900

    Clean function definitions

--- src/clj_imgcat/core.clj
+++ src/clj_imgcat/core.clj
@@ -1,16 +1,25 @@
 (ns clj-imgcat.core
   (:require [clojure.java.io :as io]))
 
-(defn slurp-bytes
-  "Slurp the bytes from a slurpable thing
-  https://clojuredocs.org/clojure.core/slurp"
-  [x]
-  (with-open [out (java.io.ByteArrayOutputStream.)]
-    (io/copy (io/input-stream x) out)
+(defn ->bytes
+  "read a file into a byte array"
+  [file]
+  (with-open [in (io/input-stream file)
+              out (java.io.ByteArrayOutputStream.)]
+    (io/copy in out)
     (.toByteArray out)))
 
-(defn imgcat [x]
-  "Display images with the iTerm2 using the Inline Images Protocol"
-  (->> (slurp-bytes x)
-       (.encodeToString (java.util.Base64/getEncoder))
-       (printf (str "\033]1337;File=;inline=1:%s" (char 7)))))
+(defn ->base64-string
+  "Encodes the byte array into a base64 string"
+  [bytes]
+  (.encodeToString (java.util.Base64/getEncoder) bytes))
+
+(defn display
+  "Displays an image using Inline Image Protocol for iTerm2"
+  [base64-string]
+  (println (str "\033]1337;File=;inline=1:" base64-string "\007")))
+
+(defn imgcat
+  "Displays an image within a terminal."
+  [file]
+  (display (->base64-string (->bytes file))))
