b4d7ef45305cbd36a5b76965f2bca87d604491e7
hisaitami <hisaitami@gmail.com>
2020-02-09 18:18:12 +0900

    Add test

---
+++ test/clj_imgcat/core_test.clj
@@ -0,0 +1,37 @@
+(ns clj-imgcat.core-test
+  (:require [clojure.test :refer :all]
+            [clojure.string :as str]
+            [clojure.java.shell :refer [sh]]
+            [clj-imgcat.core :refer :all]))
+
+(def test-file "README.md")
+
+(defn- wc [option file]
+  (-> (sh "wc" option file)
+      :out (str/split #"\s+") second Integer/parseInt))
+
+(defn- base64 [file]
+  (-> (sh "base64" file)
+      :out str/trim-newline))
+
+(deftest ->bytes-test
+  (testing "Load a local file as byte-array"
+    (is (= (-> test-file ->bytes count)
+           (wc "-c" test-file)))))
+
+(deftest ->base64-string-test
+  (testing "Encode using Base64 representation"
+    (is (= (-> test-file ->bytes ->base64-string)
+           (base64 test-file)))))
+
+(deftest display-test
+  (testing "Inline Image Protocol"
+    (let [base64-string (base64 test-file)]
+      (is (= (str "\033]1337;File=;inline=1:" base64-string "\007" \newline)
+             (with-out-str (display base64-string)))))))
+
+(deftest imgcat-test
+  (testing "imgcat logic"
+    (let [base64-string (base64 test-file)]
+      (is (= (str "\033]1337;File=;inline=1:" base64-string "\007" \newline)
+             (with-out-str (imgcat test-file)))))))
