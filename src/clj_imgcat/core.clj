2b8ed42d1764491309ed218399d8ffcffdecf830
hisaitami <hisaitami@gmail.com>
2023-01-02 21:16:22 +0900

    Parse imgcat options width, height and preserveAspectRatio

--- src/clj_imgcat/core.clj
+++ src/clj_imgcat/core.clj
@@ -14,15 +14,27 @@
   [^bytes bytes]
   (.encodeToString (java.util.Base64/getEncoder) bytes))
 
+(defn parse-options [{:keys [:width :height :preserveAspectRatio] :as options}]
+  (->> options
+       (map (fn [[k v]] (str (name k) "=" v ";")))
+       (apply str)))
+
 (defn display
   "Displays an image using Inline Image Protocol for iTerm2"
-  [base64-string]
-  (println (str "\033]1337;File=;inline=1:" base64-string "\007")))
+  ([base64-string]
+   (display base64-string nil))
+  ([base64-string options]
+   (println (str "\033]1337;"
+                 "File=;" (parse-options options) "inline=1:"
+                 base64-string "\007"))))
 
 (defn imgcat
   "Displays an image within a terminal."
-  [file]
-  (display (bytes->base64-string (file->bytes file))))
+  ([file] (imgcat file nil))
+  ([file options]
+   (let [bytes (file->bytes file)
+         base64str (bytes->base64-string bytes)]
+     (display base64str options))))
 
 (defn -main [& args]
   (if-let [file (first args)]
