3f251ac15d9e7fd16c9aa825a84b2c52e9d29ffe
hisaitami <hisaitami@gmail.com>
2023-01-06 11:31:54 +0900

    Cleanup display-test and imgcat-test

--- test/clj_imgcat/core_test.clj
+++ test/clj_imgcat/core_test.clj
@@ -39,21 +39,24 @@
     (is (= (parse-options {:foo 1}) "")))
     (is (= (parse-options {:foo 1 :width 21 :height 23}) ";width=21;height=23"))))
 
+(defn- inline-image-protocol
+  ([file]
+   (inline-image-protocol file nil))
+  ([file opts]
+   (str "\033]1337;File=inline=1"
+        ";size=" (wc "-c" test-file)
+        ";name=" (base64-string test-file)
+        opts
+        ":" (base64-file test-file) "\007"
+        \newline)))
+
 (deftest display-test
   (testing "Inline Image Protocol"
-    (is (= (str "\033]1337;File=inline=1"
-                ";size=" (wc "-c" test-file)
-                ";name=" (base64-string test-file)
-                ":" (base64-file test-file) "\007"
-                \newline)
+    (is (= (inline-image-protocol test-file)
            (with-out-str (display test-file))))))
 
 (deftest imgcat-test
   (testing "imgcat logic"
-    (is (= (str "\033]1337;File=inline=1"
-                ";size=" (wc "-c" test-file)
-                ";name=" (base64-string test-file)
-                ";width=11;height=13;preserveAspectRatio=0"
-                ":" (base64-file test-file) "\007"
-                \newline)
+    (is (= (inline-image-protocol test-file";width=11;height=13;preserveAspectRatio=0")
            (with-out-str (display test-file :width 11 :height 13 :preserveAspectRatio 0))))))
+
