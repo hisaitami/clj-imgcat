2653d6f8d0662f85e8b10af8dbf59acfa7a3ba8e
hisaitami <hisaitami@gmail.com>
2024-05-02 20:50:04 +0900

    Upgrade clojure to version 1.11.3

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/hisaitami/clj-imgcat"
   :license {:name "MIT License"
             :url "https://github.com/hisaitami/clj-imgcat/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.3"]]
   :repl-options {:init-ns clj-imgcat.core}
   :aot [clj-imgcat.core]
   :main clj-imgcat.core)
